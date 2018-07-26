7391da4eac57a11207641942b87aa62f1d33f39e
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2018-07-25 20:46:52 -1000

    Hooray

--- js/app.js
+++ js/app.js
@@ -12,7 +12,13 @@ findMenu[i].addEventListener("click",showSecret);
 }
 
 function showSecret() {
-    console.log("hello")
+    var childElem = this.querySelectorAll(".inner")[0];
+    if ( childElem.style.display=== "block") {
+        childElem.style.display = "none";
+    }else {
+        childElem.style.display = "block";
+    }
+     
    
    }
 
@@ -22,24 +28,32 @@ function showSecret() {
 
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
-var secMenu = document.querySelectorAll("menu")
+var secMenu = document.getElementsByClassName("name")
 for (var i = 0; i<secMenu.length; i++) {
-    secMenu[i].addEventListener("toggle",reveal);
+    secMenu[i].addEventListener("mouseover",reveal);
 }
 function reveal() {
-console.log("hello");
+var sMen = this.querySelectorAll(".menu")[0];
+if (sMen.style.display === "none") {
+    sMen.style.display = "block";
+}else {
+    sMen.style.display = "none";
 }
+} 
 
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/
-var thumbsDown = document.querySelectorAll(".thumb");
+var thumbsDown = document.getElementsByClassName("thumb");
 for( var i = 0;i<thumbsDown.length; i++){
 thumbsDown[i].addEventListener("click",tally)
 //console.log("Hello");
+var counter = document.createElement("div");
+counter.className = "hater";
+thumbsDown[i].appendChild(counter);
 }
 function tally(){
-var count = 0;{
-      if(thumbsDown[i]=== true);{
-    console.log(count ++);
+var haterCount = this.querySelectorAll(".hater")[0];
+//console.log(haterCount);
+haterCount.innerHTML++;
       }
-}
-}
+
+
